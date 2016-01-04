a4772f872e0a080b0a8c66a8663dc774bf0b0cb9
Ross Kaffenberger <rosskaff@gmail.com>
2016-01-04 11:53:17 -0500

    Additional unit specs for Clock#tick

--- spec/montrose/clock_spec.rb
+++ spec/montrose/clock_spec.rb
@@ -55,12 +55,52 @@ describe Montrose::Clock do
       clock.must_have_tick 1.day
     end
 
-    it "emits 1 day increments for :mday"
-    it "emits 1 day increments for :yday"
-    it "emits day interval increments"
-    it "emits 1 week increments"
-    it "emits week interval increments"
-    it "emits 1 year increments"
-    it "emits year interval increments"
+    it "emits 1 day increments for :mday" do
+      clock = new_clock(every: :month, mday: [1, -1])
+
+      clock.must_have_tick 1.day
+    end
+
+    it "emits 1 day increments for :yday" do
+      clock = new_clock(every: :year, yday: [1, 10, 100])
+
+      clock.must_have_tick 1.day
+    end
+
+    it "emits day interval increments" do
+      clock = new_clock(every: :day, interval: 3)
+
+      clock.must_have_tick 3.days
+    end
+
+    it "emits 1 day iincrements when day smallest part" do
+      clock = new_clock(every: :month, day: :tuesday)
+
+      clock.must_have_tick 1.day
+    end
+
+    it "emits 1 week increments" do
+      clock = new_clock(every: :week)
+
+      clock.must_have_tick 1.week
+    end
+
+    it "emits week interval increments" do
+      clock = new_clock(every: :week, interval: 2)
+
+      clock.must_have_tick 2.weeks
+    end
+
+    it "emits 1 year increments" do
+      clock = new_clock(every: :year)
+
+      clock.must_have_tick 1.year
+    end
+
+    it "emits year interval increments" do
+      clock = new_clock(every: :year, interval: 10)
+
+      clock.must_have_tick 10.years
+    end
   end
 end
