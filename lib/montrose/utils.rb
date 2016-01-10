4bc7266ff4df1ba35b70f0fec033324be1568109
Ross Kaffenberger <rosskaff@gmail.com>
2016-01-10 18:14:42 -0500

    Remove unused code

--- lib/montrose/utils.rb
+++ lib/montrose/utils.rb
@@ -28,47 +28,5 @@ module Montrose
         raise "Did not recognize day #{name}"
       end
     end
-
-    def days_in(period, time)
-      send("days_in_#{period}", time)
-    end
-
-    # Get the days in the month for +time
-    def days_in_month(time)
-      date = Date.new(time.year, time.month, 1)
-      ((date >> 1) - date).to_i
-    end
-
-    # Get the days in the month for +time
-    def days_in_year(time)
-      date = time.to_date
-      ((date + 1.year) - date).to_i
-    end
-
-    # def nth_day_of_year?(nth_occ, time)
-    #   curr_wday = time.wday
-    #   nth_day = time.yday
-    #   first_wday = time.beginning_of_year.wday
-    #   total_days = days_in_year(time)
-    #
-    #   nth_day_from_start_or_end?(nth_occ, curr_wday, nth_day, first_wday, total_days)
-    # end
-    #
-    # def nth_day_of_month?(nth_occ, time)
-    #   curr_wday = time.wday
-    #   nth_day = time.mday
-    #   first_wday = time.beginning_of_month.wday
-    #   total_days = days_in_month(time)
-    #
-    #   nth_day_from_start_or_end?(nth_occ, curr_wday, nth_day, first_wday, total_days)
-    # end
-    #
-    # def nth_day_from_start_or_end?(nth_occ, curr_wday, nth_day, first_wday, total_days)
-    #   first_occ = ((7 - first_wday) + curr_wday) % 7 + 1
-    #   total_occ = ((total_days - first_occ + 1) / 7.0).ceil
-    #   curr_occ = (curr_wday - first_occ) / 7 + 1
-    #
-    #   nth_occ == curr_occ || (nth_occ < 0 && (total_occ + nth_occ + 1) == curr_occ)
-    # end
   end
 end
