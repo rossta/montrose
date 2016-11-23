f4669d9080a9e5d3326839086bb6d402ea47a9c6
Ross Kaffenberger <rosskaff@gmail.com>
2016-11-23 16:52:12 -0500

    Add docs to schedule

--- lib/montrose/schedule.rb
+++ lib/montrose/schedule.rb
@@ -1,7 +1,25 @@
 module Montrose
+  # A schedule represents a group of recurrences
+  #
+  # @author Ross Kaffenberger
+  # @since 0.0.1
+  # @attr_reader [Array] rules the list of recurrences
+  #
   class Schedule
     attr_accessor :rules
 
+    # Instantiates a schedule and yields the instance to an optional
+    # block for building recurrences inline
+    #
+    # @example Build a schedule with multiple rules added in the given block
+    #
+    # schedule = Montrose::Schedule.build do |s|
+    #   s << { every: :day }
+    #   s << { every: :year }
+    # end
+    #
+    # @return [Montrose::Schedule]
+    #
     def self.build
       schedule = new
       yield schedule if block_given?
@@ -12,6 +30,20 @@ module Montrose
       @rules = []
     end
 
+    # Add a recurrence rule to the schedule, either by hash or recurrence
+    # instance
+    #
+    # @example Add a recurrence by hash
+    #
+    # schedule = Montrose::Schedule.new
+    # schedule << { every: :day }
+    #
+    # @example Add a recurrence by instance
+    #
+    # schedule = Montrose::Schedule.new
+    # recurrence = Montrose.recurrence(every: :day)
+    # schedule << recurrence
+    #
     def <<(rule)
       @rules << Montrose::Recurrence.new(rule)
 
@@ -19,10 +51,25 @@ module Montrose
     end
     alias add <<
 
-    def include?(time)
-      @rules.any? { |r| r.include?(time) }
+    # Return true/false if given timestamp is included in any of the rules
+    # found in the schedule
+    #
+    # @return [Boolean] whether or not timestamp is included in schedule
+    #
+    def include?(timestamp)
+      @rules.any? { |r| r.include?(timestamp) }
     end
 
+    # Returns an enumerator for iterating over timestamps in the schedule
+    #
+    # @example Return the events
+    #   schedule = Montrose::Schedule.build do |s|
+    #     s << { every: :day }
+    #   end
+    #   schedule.events
+    #
+    # @return [Enumerator] an enumerator of recurrence timestamps
+    #
     def events(opts = {})
       enums = @rules.map { |r| r.merge(opts).events }
       Enumerator.new do |y|
