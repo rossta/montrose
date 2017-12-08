ed48ec32950000c08a2bbdb603c0a9f042c69f62
Ross Kaffenberger <rosskaff@gmail.com>
2017-12-08 11:39:16 -0500

    Additional Rubocop corrections

--- spec/support/trace.rb
+++ spec/support/trace.rb
@@ -4,9 +4,9 @@ if ENV["TRACE"]
   $stack_size = ENV["TRACE"].to_i
   $trace_out = open("trace.txt", "w")
 
-  set_trace_func proc { |event, file, line, id, _binding, classname|
+  set_trace_func(proc { |event, file, line, id, _binding, classname|
     if event == "call" && caller.length > $stack_size
       $trace_out.puts "#{file}:#{line} #{classname}##{id}"
     end
-  }
+  })
 end
