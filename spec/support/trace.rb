3e4b4ecb2692882c7d0cac5ccbdf2a00428e69a0
Ross Kaffenberger <rosskaff@gmail.com>
2018-02-25 08:58:18 -0500

    Bump rubocop 0.52.1

--- spec/support/trace.rb
+++ spec/support/trace.rb
@@ -5,8 +5,6 @@ if ENV["TRACE"]
   $trace_out = open("trace.txt", "w")
 
   set_trace_func(proc { |event, file, line, id, _binding, classname|
-    if event == "call" && caller.length > $stack_size
-      $trace_out.puts "#{file}:#{line} #{classname}##{id}"
-    end
+    $trace_out.puts "#{file}:#{line} #{classname}##{id}" if event == "call" && caller.length > $stack_size
   })
 end
