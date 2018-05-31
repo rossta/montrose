5f02214188832cf957746993faaae0c7fc9150f8
Jeff Stubler <brunsa2@gmail.com>
2018-05-30 20:43:23 -0500

    Fix day and month number parsing when strings

--- spec/montrose/utils_spec.rb
+++ spec/montrose/utils_spec.rb
@@ -82,6 +82,18 @@ describe Montrose::Utils do
     it { month_number!(10).must_equal 10 }
     it { month_number!(11).must_equal 11 }
     it { month_number!(12).must_equal 12 }
+    it { month_number!("1").must_equal 1 }
+    it { month_number!("2").must_equal 2 }
+    it { month_number!("3").must_equal 3 }
+    it { month_number!("4").must_equal 4 }
+    it { month_number!("5").must_equal 5 }
+    it { month_number!("6").must_equal 6 }
+    it { month_number!("7").must_equal 7 }
+    it { month_number!("8").must_equal 8 }
+    it { month_number!("9").must_equal 9 }
+    it { month_number!("10").must_equal 10 }
+    it { month_number!("11").must_equal 11 }
+    it { month_number!("12").must_equal 12 }
     it { -> { month_number!(:foo) }.must_raise Montrose::ConfigurationError }
     it { -> { month_number!("foo") }.must_raise Montrose::ConfigurationError }
     it { -> { month_number!(0) }.must_raise Montrose::ConfigurationError }
@@ -110,6 +122,13 @@ describe Montrose::Utils do
     it { day_number!(4).must_equal 4 }
     it { day_number!(5).must_equal 5 }
     it { day_number!(6).must_equal 6 }
+    it { day_number!("0").must_equal 0 }
+    it { day_number!("1").must_equal 1 }
+    it { day_number!("2").must_equal 2 }
+    it { day_number!("3").must_equal 3 }
+    it { day_number!("4").must_equal 4 }
+    it { day_number!("5").must_equal 5 }
+    it { day_number!("6").must_equal 6 }
     it { -> { day_number!(-3) }.must_raise Montrose::ConfigurationError }
     it { -> { day_number!(:foo) }.must_raise Montrose::ConfigurationError }
     it { -> { day_number!("foo") }.must_raise Montrose::ConfigurationError }
