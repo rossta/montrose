0cb5e8df59058057e3dc0fc006e259e9d5c05258
Ross Kaffenberger <rosskaff@gmail.com>
2021-02-02 11:47:21 -0500

    Fix for date range logic in Montrose.covering

--- spec/montrose/rule/covering_spec.rb
+++ spec/montrose/rule/covering_spec.rb
@@ -3,17 +3,65 @@
 require "spec_helper"
 
 describe Montrose::Rule::Covering do
-  let(:rule) { Montrose::Rule::Covering.new(1.day.ago..3.days.from_now) }
+  let(:now) { Time.new(2015, 9, 3, 12, 0, 0, "-04:00") }
 
-  describe "#include?" do
-    it { refute rule.include?(time_now - 10.days) }
-    it { assert rule.include?(time_now) }
-    it { refute rule.include?(time_now + 10.days) }
+  let(:starts) { now - 1.days }
+  let(:ends)   { now + 3.days }
+
+  describe "time range" do
+    let(:rule) { Montrose::Rule::Covering.new(starts..ends) }
+
+    describe "#include?" do
+      it { refute rule.include?(now - 10.days) }
+      it { assert rule.include?(now) }
+      it { refute rule.include?(now + 10.days) }
+    end
+
+    describe "#continue?" do
+      it { assert rule.continue?(now - 10.days) }
+      it { assert rule.continue?(now) }
+      it { refute rule.continue?(now + 10.days) }
+    end
   end
 
-  describe "#continue?" do
-    it { assert rule.continue?(time_now - 10.days) }
-    it { assert rule.continue?(time_now) }
-    it { refute rule.continue?(time_now + 10.days) }
+  describe "date range" do
+    let(:rule) { Montrose::Rule::Covering.new((starts.to_date)..(ends.to_date)) }
+
+    describe "#include?" do
+      it { refute rule.include?(now - 10.days) }
+
+      # Testing time zone differences
+      it { refute rule.include?(Time.new(2015, 9, 1, 19, 0, 0, "-04:00")) }
+      it { refute rule.include?(Time.new(2015, 9, 1, 20, 0, 0, "-04:00")) }
+      it { refute rule.include?(Time.new(2015, 9, 1, 21, 0, 0, "-04:00")) }
+
+      it { refute rule.include?(Time.new(2015, 9, 1, 23, 0, 0, "-04:00")) }
+      it { assert rule.include?(Time.new(2015, 9, 2, 00, 0, 0, "-04:00")) }
+      it { assert rule.include?(Time.new(2015, 9, 2, 01, 0, 0, "-04:00")) }
+
+      it { assert rule.include?(now - 1.days) }
+
+      it { assert rule.include?(now) }
+      it { assert rule.include?(now + 1.days) }
+
+      it { assert rule.include?(Time.new(2015, 9, 6, 19, 0, 0, "-04:00")) }
+      it { assert rule.include?(Time.new(2015, 9, 6, 20, 0, 0, "-04:00")) }
+      it { assert rule.include?(Time.new(2015, 9, 6, 21, 0, 0, "-04:00")) }
+
+      it { assert rule.include?(Time.new(2015, 9, 6, 23, 0, 0, "-04:00")) }
+      it { refute rule.include?(Time.new(2015, 9, 7, 00, 0, 0, "-04:00")) }
+      it { refute rule.include?(Time.new(2015, 9, 7, 01, 0, 0, "-04:00")) }
+
+      it { refute rule.include?(now + 10.days) }
+    end
+
+    describe "#continue?" do
+      it { assert rule.continue?(now - 10.days) }
+      it { assert rule.continue?(now - 1.days) }
+      it { assert rule.continue?(now) }
+      it { assert rule.continue?(now + 1.days) }
+      it { refute rule.continue?(now + 3.days) }
+      it { refute rule.continue?(now + 10.days) }
+    end
   end
 end
