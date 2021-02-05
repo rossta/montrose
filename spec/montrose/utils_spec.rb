80ab17c4988ca673555095cc24e194151d75527e
Ross Kaffenberger <rosskaff@gmail.com>
2021-02-05 16:26:26 -0500

    Extract Montrose::Month for month number parsing

--- spec/montrose/utils_spec.rb
+++ spec/montrose/utils_spec.rb
@@ -45,61 +45,6 @@ describe Montrose::Utils do
     end
   end
 
-  describe "#month_number!" do
-    it { month_number!(:january).must_equal 1 }
-    it { month_number!(:february).must_equal 2 }
-    it { month_number!(:march).must_equal 3 }
-    it { month_number!(:april).must_equal 4 }
-    it { month_number!(:may).must_equal 5 }
-    it { month_number!(:june).must_equal 6 }
-    it { month_number!(:july).must_equal 7 }
-    it { month_number!(:august).must_equal 8 }
-    it { month_number!(:september).must_equal 9 }
-    it { month_number!(:october).must_equal 10 }
-    it { month_number!(:november).must_equal 11 }
-    it { month_number!(:december).must_equal 12 }
-    it { month_number!("january").must_equal 1 }
-    it { month_number!("february").must_equal 2 }
-    it { month_number!("march").must_equal 3 }
-    it { month_number!("april").must_equal 4 }
-    it { month_number!("may").must_equal 5 }
-    it { month_number!("june").must_equal 6 }
-    it { month_number!("july").must_equal 7 }
-    it { month_number!("august").must_equal 8 }
-    it { month_number!("september").must_equal 9 }
-    it { month_number!("october").must_equal 10 }
-    it { month_number!("november").must_equal 11 }
-    it { month_number!("december").must_equal 12 }
-    it { month_number!(1).must_equal 1 }
-    it { month_number!(2).must_equal 2 }
-    it { month_number!(3).must_equal 3 }
-    it { month_number!(4).must_equal 4 }
-    it { month_number!(5).must_equal 5 }
-    it { month_number!(6).must_equal 6 }
-    it { month_number!(7).must_equal 7 }
-    it { month_number!(8).must_equal 8 }
-    it { month_number!(9).must_equal 9 }
-    it { month_number!(10).must_equal 10 }
-    it { month_number!(11).must_equal 11 }
-    it { month_number!(12).must_equal 12 }
-    it { month_number!("1").must_equal 1 }
-    it { month_number!("2").must_equal 2 }
-    it { month_number!("3").must_equal 3 }
-    it { month_number!("4").must_equal 4 }
-    it { month_number!("5").must_equal 5 }
-    it { month_number!("6").must_equal 6 }
-    it { month_number!("7").must_equal 7 }
-    it { month_number!("8").must_equal 8 }
-    it { month_number!("9").must_equal 9 }
-    it { month_number!("10").must_equal 10 }
-    it { month_number!("11").must_equal 11 }
-    it { month_number!("12").must_equal 12 }
-    it { -> { month_number!(:foo) }.must_raise Montrose::ConfigurationError }
-    it { -> { month_number!("foo") }.must_raise Montrose::ConfigurationError }
-    it { -> { month_number!(0) }.must_raise Montrose::ConfigurationError }
-    it { -> { month_number!(13) }.must_raise Montrose::ConfigurationError }
-  end
-
   describe "#day_number!" do
     it { day_number!(:sunday).must_equal 0 }
     it { day_number!(:monday).must_equal 1 }
